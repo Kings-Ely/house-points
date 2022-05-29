3a3e32865512816dc22974e45f72b38326a49351
revers3ntropy <josephcoppin@gmail.com>
2022-05-29 19:58:23 +0100

    submit house points working

--- dist/backend/submit-hp.php
+++ dist/backend/submit-hp.php
@@ -3,5 +3,17 @@
 require('sql.php');
 
 queries(function ($query) {
-    $query('INSERT INTO housepoints (student, description) VALUES (?, ?)', 'is', $_GET['student'], $_GET['description']);
+    $studentID = $query(
+        'SELECT id FROM students WHERE code=?',
+        's',
+        $_GET['student']
+    ) -> fetch_array(MYSQLI_ASSOC) ['id'];
+    if (!$studentID) {
+        die('invalid code');
+    }
+
+    $query(
+        'INSERT INTO housepoints (student, description) VALUES (?, ?)',
+        'is', $studentID, $_GET['description']
+    );
 });
