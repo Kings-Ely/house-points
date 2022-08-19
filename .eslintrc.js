c1ca2d94c3cbeddfbbca0b2cc499b453ef098431
revers3ntropy <josephcoppin@gmail.com>
2022-08-19 16:57:25 +0100

    prettier and eslint and mdlint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig-eslint.json'],
+        extraFileExtensions: ['.json']
+    },
+    plugins: ['prettier'],
+    env: {
+        node: true
+    },
+    extends: ['plugin:prettier/recommended'],
+    settings: {
+        'import/resolver': {
+            typescript: {}
+        }
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'no-console': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/explicit-member-accessibility': 'error'
+    },
+    overrides: [
+        {
+            files: ['test/**/*.ts'],
+            rules: {
+                'no-console': 0
+            }
+        }
+    ]
+};
